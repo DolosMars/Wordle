3ed99004e7df07e4cc974279984282939460289b
DolosMars <high106huiyu@gmail.com>
2022-03-23 18:27:14 +0800

    Initial Commit

---
+++ test032301/test032301App.swift
@@ -0,0 +1,17 @@
+//
+//  test032301App.swift
+//  test032301
+//
+//  Created by User06 on 2022/3/23.
+//
+
+import SwiftUI
+
+@main
+struct test032301App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
